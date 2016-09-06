33b146ff5e4b35cea41ff62bb29aa2b2bf98169c
Will Landau <will.landau@gmail.com>
2016-09-05 21:54:05 -0400

    Add multiple reps per dataset in example 2

--- inst/example2/workflow.R
+++ inst/example2/workflow.R
@@ -1,22 +1,22 @@
 library(dplyr)
 library(remakeGenerator)
 
+# Generate 6 datasets: 2 replicates for each of following commands.
 datasets = commands(
   normal16 = normal_dataset(n = 16),
   poisson32 = poisson_dataset(n = 32),
   poisson64 = poisson_dataset(n = 64)
-)
+) %>%
+expand(values = c("rep1", "rep2"))
 
-################################
-### DIFFERENT FROM EXAMPLE 1 ###
-################################
-
+# Same as the analyses() function.
 analyses = commands(
   linear = linear_analysis(..dataset..),
   quadratic = quadratic_analysis(..dataset..)
 ) %>% 
 evaluate(wildcard = "..dataset..", values = datasets$target)
 
+# Same as the summaries() function.
 summaries = commands(
   mse = mse_summary(..dataset.., ..analysis..),
   coef = coefficients_summary(..analysis..)
@@ -24,12 +24,9 @@ summaries = commands(
 evaluate(wildcard = "..analysis..", values = analyses$target) %>% 
 evaluate(wildcard = "..dataset..", values = datasets$target, expand_x = FALSE)
 
-################################
-###    END OF DIFFERENCES    ###
-################################
-
-mse = gather(summaries[1:6,], target = "mse")
-coef = gather(summaries[7:12,], target = "coef")
+# 24 datasets to summarize rather than 12.
+mse = gather(summaries[1:12,], target = "mse")
+coef = gather(summaries[13:24,], target = "coef")
 
 output = commands(
   coef_table = do.call(I("rbind"), coef),
@@ -43,7 +40,7 @@ plots = commands(
 plots$plot = TRUE
 
 reports = data.frame(target = strings(markdown.md, latex.tex),
-  depends = c("poisson32, coef_table, coef.csv", ""))
+  depends = c("poisson32_rep1, coef_table, coef.csv", ""))
 reports$knitr = TRUE
 
 begin = c("# This is my Makefile", "# Variables...")
