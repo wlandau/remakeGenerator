11f745fc891385f72cc4695328df2d56391c6542
Will Landau <will.landau@gmail.com>
2016-09-14 21:56:00 -0400

    Unit test makefile=NULL

--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -12,3 +12,17 @@ test_that("Function workflow() is correct.", {
     readLines(file.path("..", "test-workflow", "remake.yml")))
   testrm("workflow")
 })
+
+test_that("Function workflow(..., makefile = NULL) turns off the makefile.", {
+  testwd("workflow-no-makefile")
+  dat = commands(
+    normal16 = normal_dataset(n = 16),
+    poisson32 = poisson_dataset(n = 32),
+    poisson64 = poisson_dataset(n = 64))
+  targ = targets(dat = dat)
+  workflow(targ, makefile = NULL)
+  expect_equal(readLines("remake.yml"), 
+    readLines(file.path("..", "test-workflow", "remake-no-makefile.yml")))
+  expect_false(file.exists("Makefile"))
+  testrm("workflow-no-makefile")
+})
