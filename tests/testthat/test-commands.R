59cf2080375cfaa88e1c2334ae1ef2eb1fa8a91d
Will Landau <will.landau@gmail.com>
2016-09-17 23:04:28 -0400

    Add assert_commands tests. hopefully 100%

--- tests/testthat/test-commands.R
+++ tests/testthat/test-commands.R
@@ -4,6 +4,8 @@ source("utils.R")
 
 test_that("Function commands() is correct.", {
   testwd("commands")
+  expect_error(assert_commands(data.frame(x = 1:2, command = 1:2)))
+  expect_error(assert_commands(data.frame(command = 1:2, target = rep(1, 2))))
   expect_equal(commands(x = 1, y = f(2)), 
     data.frame(target = c("x", "y"), command = c(1, "f(2)"), stringsAsFactors = F))
   testrm("commands")
