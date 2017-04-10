e1b3a947cdd351b89398673377b8c140741ba262
Kendon Bell <kendonB@users.noreply.github.com>
2017-04-09 19:51:03 -0700

    Added option to not run a command

--- R/ui.R
+++ R/ui.R
@@ -88,38 +88,40 @@ targets = function(...){
 
 #' @title Function \code{workflow}
 #' @description Write and execute a Makefile to run a remake workflow.
-#' Use the \code{\link{help_remakeGenerator}} function to get more help.
+#'   Use the \code{\link{help_remakeGenerator}} function to get more help.
 #' @details Use the \code{\link{help_remakeGenerator}} function to get more help.
 #' @seealso \code{\link{help_remakeGenerator}}
 #' @export 
 #' @param targets YAML-like list of targets, which you can generate by supplying
-#' data frames of remake commands to the \code{\link{targets}} function.
+#'   data frames of remake commands to the \code{\link{targets}} function.
 #' @param make_these names of the remake targets to makle
 #' @param sources Character vector of R source files or their containing folders.
 #' @param packages Character vector of packages
 #' @param remakefile Character, name of the \code{remake} file to generate. 
-#' Should be in the current working directory.
+#'   Should be in the current working directory.
 #' @param makefile Deprecated. The name of the Makefile will always be 'Makefile'.
 #' @param prepend Character vector of lines to prepend to the Makefile
 #' @param begin Use "prepend" instead.
 #' @param clean Deprecated. The Makefile is no longer stand alone, so it does not need 'clean'.
-#' Use \code{remake::make("clean")}.
+#'   Use \code{remake::make("clean")}.
 #' @param remake_args Fully-named list of additional arguments to \code{remake::make}.
-#' You cannot set \code{target_names} or \code{remake_file} this way because those
-#' names are reserved.
+#'   You cannot set \code{target_names} or \code{remake_file} this way because those
+#'   names are reserved.
 #' @param run logical, whether to actually run the Makefile or just write it.
 #' @param command character scalar, command to run to execute the Makefile.
-#' \code{command} and \code{args} will be used to call 
-#' \code{system2(command = command, args = args)} to run the \code{Makefile}.
-#' For example, to execute the \code{Makefile} using 4 parallel jobs
-#' while suppressing output to the console, use 
-#' \code{makefile(..., command = "make", args = c("--jobs=4", "-s"))}.
+#'   \code{command} and \code{args} will be used to call 
+#'   \code{system2(command = command, args = args)} to run the \code{Makefile}.
+#'   For example, to execute the \code{Makefile} using 4 parallel jobs
+#'   while suppressing output to the console, use 
+#'   \code{makefile(..., command = "make", args = c("--jobs=4", "-s"))}.
+#'   Passing \code{command = NULL} will generate the yaml file and not run a
+#'   command.
 #' @param args character vector of arguments to the specified \code{command}.
-#' \code{command} and \code{args} will be used to call
-#' \code{system2(command = command, args = args)} to run the \code{Makefile}.
-#' For example, to execute the \code{Makefile} using 4 parallel jobs
-#' while suppressing output to the console, use
-#' \code{makefile(..., command = "make", args = c("--jobs=4", "-s"))}.
+#'   \code{command} and \code{args} will be used to call
+#'   \code{system2(command = command, args = args)} to run the \code{Makefile}.
+#'   For example, to execute the \code{Makefile} using 4 parallel jobs
+#'   while suppressing output to the console, use
+#'   \code{makefile(..., command = "make", args = c("--jobs=4", "-s"))}.
 workflow = function(targets = NULL, make_these = "all", sources = NULL, packages = NULL,
   remakefile = "remake.yml", makefile = NULL, prepend = NULL,
   begin = NULL, clean = NULL, remake_args = list(verbose = TRUE), run = TRUE,
@@ -134,7 +136,9 @@ workflow = function(targets = NULL, make_these = "all", sources = NULL, packages
   write(as.yaml(yaml), remakefile)
   yaml_yesno_truefalse(remakefile)
   packages = unique(c("methods", packages))
-  makefile(targets = make_these, remakefiles = remakefile, 
+  if(!is.null(command)){
+    makefile(targets = make_these, remakefiles = remakefile, 
            prepend = prepend, remake_args = remake_args, run = run,
            command = command, args = args)
+  }
 }
