9f6e8c96d97b4406653ce55f207e686a52400e2e
Kendon Bell <kendonB@users.noreply.github.com>
2017-04-11 11:19:14 -0700

    Update test-assert_commands.R

--- tests/testthat/test-assert_commands.R
+++ tests/testthat/test-assert_commands.R
@@ -5,6 +5,6 @@ source("utils.R")
 test_that("Function assert_commands() is correct.", {
   testwd("assert_commands")
   expect_error(assert_commands())
-  expect_equal(assert_commands(commands(x = 1, y = f(2))), NULL)
+  expect_equal(assert_commands(commands(x = 1, y = f(2))), commands(x = 1, y = f(2)))
   testrm("assert_commands")
 })
